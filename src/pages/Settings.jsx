fa70e49a10964e5d85410f4b2a8c85dd6f811732
HabiburRahmanAnik <habib264521@gmail.com>
2022-11-03 00:16:25 +0600

    .

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Settings = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabChange = (index) => {
+    setActiveIndex(index);
+    console.log(index);
+  };
+
   return (
-    <div className="text-center text-2xl pt-24 font-semibold">Coming soon</div>
+    <div className="text-center text-2xl pt-24 flex gap-2">
+      <button
+        className="px-4 py-1 bg-gray-200"
+        onClick={() => handleTabChange(0)}
+      >
+        a
+      </button>
+      <button
+        className="px-4 py-1 bg-gray-200"
+        onClick={() => handleTabChange(1)}
+      >
+        b
+      </button>
+      <button
+        className="px-4 py-1 bg-gray-200"
+        onClick={() => handleTabChange(2)}
+      >
+        c
+      </button>
+    </div>
   );
 };
 
