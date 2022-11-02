c3375e9faabc91df456bb99973ede543862750e1
HabiburRahmanAnik <habib264521@gmail.com>
2022-11-03 00:32:32 +0600

    active tab color

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -12,24 +12,24 @@ const Settings = () => {
   return (
     <div className="text-center text-2xl pt-24 flex gap-2">
       <button
-        className="px-4 py-1 bg-gray-200"
+        className={`px-4 py-1 bg-gray-200 ${activeIndex === 0 && 'bg-red-300'}`}
         onClick={() => handleTabChange(0)}
       >
         a
       </button>
       <button
-        className="px-4 py-1 bg-gray-200"
+        className={`px-4 py-1 bg-gray-200 ${activeIndex === 1 && 'bg-red-300'}`}
         onClick={() => handleTabChange(1)}
       >
         b
       </button>
       <button
-        className="px-4 py-1 bg-gray-200"
+        className={`px-4 py-1 bg-gray-200 ${activeIndex === 2 && 'bg-red-300'}`}
         onClick={() => handleTabChange(2)}
       >
         c
       </button>
-      <Tab activeIndex={activeIndex}/>
+      <Tab activeIndex={activeIndex} />
     </div>
   );
 };
